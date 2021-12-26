dcbda3c34b294bc93a14e53f69966234d73f2929
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-12-26 17:37:17 +0530

    Refactor:nodemailer file added

--- FundooNote/app/utility/user.nodemailer.js
+++ FundooNote/app/utility/user.nodemailer.js
@@ -2,35 +2,41 @@ const nodemailer = require('nodemailer');
 require('dotenv').config
 const pool = require('../../config/database.config');
 const queries = require("..//queries/user.queries");
-const auth = require("../utility/user.authenticate");
-exports.sendEmail = (data) => {
+exports.sendEmail = (data,callback) => {
+  let code = Math.random().toString(36).substring(2, 15)
   const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     service: "gmail",
     auth: {
-      user: 'aishwaryaashokkadam',
-      pass: '8605025507'
+      user: "aishwaryaashokkadam",
+      pass:"8605025507"
     }
   });
   console.log("Jwt Token Generate");
-  const token = auth.jwtTokenGenerate(data);
+  
+  //const token = Helper.jwtTokenGenerate(data);
   const mailOptions = {
     from: process.env.EMAIL,
     to: "aishwaryaashokkadam@gmail.com",
     subject: "Reset password Link",
     html: `<h2>please click on this link to change the password</h2>
-                <p>${process.env.CLIENT_MAIL}/resetPassword/${token}</p>
+               
+                <p>${process.env.CLIENT_URL}/resetpassword/${code}</p>
                 `
   };
-
-  transporter.sendMail(mailOptions, (err, info) => {
+  
+  transporter.sendMail(mailOptions, (err, data) => {
     if (err) {
-      console.log("error",err);
-      return null;
-    } else {
-      const values = [code,process.env.EMAIL];
-            console.log(values);
-            pool.query(queries.resetUser,values);
-      };
-      return data;
-    });
-  }
+     
+      return callback(err,null);
+    } else{
+          const values = [code,process.env.EMAIL];
+          console.log(values);
+          pool.query(queries.resetUser,values);
+          };
+      return callback(null,data);
+   });
+}; 
